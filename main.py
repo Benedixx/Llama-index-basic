fd52b8a25e34e0a80c33c5dad0b29807b132dd1d
Novebri Tito Ramadhani <novebritito@gmail.com>
2024-04-10 22:42:07 +0700

    update: add simple webpage reader and do querying

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import os
 from dotenv import load_dotenv
+from llama_index.core import VectorStoreIndex
+from llama_index.readers.web import SimpleWebPageReader
+
+
+def main (url:str) -> None:
+    prompt = "who is the writer and illustrator of lightnovel konosuba?"
+    documents = SimpleWebPageReader(html_to_text=True).load_data(urls=[url])
+    index = VectorStoreIndex.from_documents(documents=documents)
+    query_engine = index.as_query_engine()
+    response = query_engine.query(prompt)
+    print("prompt : " ,prompt)
+    print("response : ", response)
+    
 if __name__ == "__main__":
     load_dotenv()
-    print(os.getenv("OPENAI_API_KEY"))
-    print("Hello World!")
+    main(url="https://en.wikipedia.org/wiki/KonoSuba")
